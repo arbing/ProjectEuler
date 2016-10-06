5df9d1c45ffe89e9ce61dab4f71004438b0883f7
ArBing Xie <xiebingting@gmail.com>
2016-10-07 03:01:29 +0800

    No 6

--- ProjectEuler/Problems/No006.cs
+++ ProjectEuler/Problems/No006.cs
@@ -19,13 +19,6 @@ Find the difference between the sum of the squares of the first one hundred natu
     {
         public long Run(long n = 100)
         {
-            //var sequence = Enumerable.Range(1, (int)n).ToArray();
-            //var squares = sequence.Select(i => i * i).ToArray();
-            //var sumOfSquares = squares.Sum();
-            //var squareOfSum = (long)Math.Pow(sequence.Sum(), 2);
-
-            //return squareOfSum - sumOfSquares;
-
             // (1 + 2 + ... + n) ^ 2 = ((n + 1) * n / 2) ^ 2
             var squareOfSum = (long)Math.Pow((n + 1) * n * 0.5, 2);
 
